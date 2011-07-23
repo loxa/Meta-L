d496b79e2fb7430001812aa1d3759979d74e2d09
loxa <loxaXcracker@hotmail.com>
2011-07-23 07:21:14 -0700

    Edited Meta-L.cpp via GitHub

--- Meta-L.cpp
+++ Meta-L.cpp
@@ -9,25 +9,27 @@ typedef unsigned char BYTE;
 void _NOACTION_(){};
 
 const char *cPath = "C:\\Users\\loxa winbox\\Desktop\\Meta-L\\HelloWorldTest.exe";	
+const char *cPathW = "C:\\Users\\loxa winbox\\Desktop\\Meta-L\\HelloWorldTest2.exe";
+
 BYTE *bFileBuffer;			
 FILE *fFile = NULL;	
+FILE *fFileW = NULL;	
+
 long fileSize;
 
 //**********************************************************
 
 
-
 BYTE* _remNOP_()
 {
-     //NULLs all NOPs (0x90->0x0)
+     //NULLs most NOPs (0x90->0x0)
      long lFileBufferSize = fileSize;
-     for(int i=0;i<=lFileBufferSize;i++)
+     for(int i=0;i<=lFileBufferSize+1;i++)
      {
-        if(bFileBuffer[i]==0x90)
-            bFileBuffer[i]=0x0;
+        if((bFileBuffer[i]==0x90) && (bFileBuffer[i+1]==0x90))
+            {bFileBuffer[i]=0x0; bFileBuffer[i+1]=0x0;}
      }
 }
-
 
 long _fFileSize(FILE *file)
 {     
@@ -39,10 +41,22 @@ long _fFileSize(FILE *file)
 	return lEndPos;
 }
 
-void _fOpen()
+void _fOpenRead()
 {
     if ((fFile = fopen(cPath, "rb+")) == NULL){_NOACTION_();} else {_NOACTION_();}
 }
+
+void _fOpenWrite()
+{
+    if ((fFileW = fopen(cPathW, "wb+")) == NULL){_NOACTION_();} else {_NOACTION_();}
+}
+
+void _fWriteFile()
+{
+    _fOpenWrite();
+    fwrite(bFileBuffer, 1, fileSize, fFileW);     
+}
+
 
 void _fPrint(BYTE *buffer, int count )
 {
@@ -52,17 +66,20 @@ void _fPrint(BYTE *buffer, int count )
 
 int main()
 {
-	_fOpen();
+	_fOpenRead();
 
 	fileSize = _fFileSize(fFile);
 	bFileBuffer = new BYTE[fileSize];
 	fread(bFileBuffer, fileSize, 1, fFile);
 	_remNOP_();
+	_fWriteFile();
 	//_fPrint(bFileBuffer,500);
 
-             
 	cin.get();
 	delete[]bFileBuffer;
+	
     fclose(fFile);   
+    fclose(fFileW); 
+    
 	system("pause");
 }
