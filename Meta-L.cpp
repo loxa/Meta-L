313b41557ba2b5cd8ac0ddd80a8f6284a35f06d7
loxa <loxaXcracker@hotmail.com>
2011-07-23 05:51:07 -0700

    Edited Meta-L.cpp via GitHub

--- Meta-L.cpp
+++ Meta-L.cpp
@@ -6,9 +6,31 @@ using namespace std;
 
 // An unsigned char can store 1 Bytes (8bits) of data (0-255)
 typedef unsigned char BYTE;
+void _NOACTION_(){};
 
-long getFileSize(FILE *file)
+const char *cPath = "C:\\Users\\loxa winbox\\Desktop\\Meta-L\\HelloWorldTest.exe";	
+BYTE *bFileBuffer;			
+FILE *fFile = NULL;	
+long fileSize;
+
+//**********************************************************
+
+
+
+BYTE* _remNOP_()
 {
+     //NULLs all NOPs (0x90->0x0)
+     long lFileBufferSize = fileSize;
+     for(int i=0;i<=lFileBufferSize;i++)
+     {
+        if(bFileBuffer[i]==0x90)
+            bFileBuffer[i]=0x0;
+     }
+}
+
+
+long _fFileSize(FILE *file)
+{     
 	long lCurPos, lEndPos;
 	lCurPos = ftell(file);
 	fseek(file, 0, 2);
@@ -17,30 +39,30 @@ long getFileSize(FILE *file)
 	return lEndPos;
 }
 
-void _print(BYTE *buffer, long fileSize )
-     {
-        for (int i = 0; i < fileSize; i++)     
+void _fOpen()
+{
+    if ((fFile = fopen(cPath, "rb+")) == NULL){_NOACTION_();} else {_NOACTION_();}
+}
+
+void _fPrint(BYTE *buffer, int count )
+{
+        for (int i = 0; i < count; i++)     
              cout << buffer[i];
-     }
+}
 
 int main()
 {
-	const char *filePath = "C:\\Dev-Cpp\\HelloWorldTest.exe";	
-	BYTE *fileBuf;			
-	FILE *file = NULL;		
+	_fOpen();
 
-	if ((file = fopen(filePath, "rb+")) == NULL)`
-		cout << "Could not open specified file" << endl;
-	else
-		cout << "File opened successfully" << endl;
-
-	long fileSize = getFileSize(file);
-	fileBuf = new BYTE[fileSize];
-	fread(fileBuf, fileSize, 1, file);
+	fileSize = _fFileSize(fFile);
+	bFileBuffer = new BYTE[fileSize];
+	fread(bFileBuffer, fileSize, 1, fFile);
+	_remNOP_();
+	//_fPrint(bFileBuffer,500);
 
              
 	cin.get();
-	delete[]fileBuf;
-    fclose(file);   
+	delete[]bFileBuffer;
+    fclose(fFile);   
 	system("pause");
 }
