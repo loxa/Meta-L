ffe6acf2aec7a8594389a09cd46069563ff13c99
loxa <loxaXcracker@hotmail.com>
2011-07-26 10:11:50 +0300

    Edited Meta-L.cpp via GitHub

--- Meta-L.cpp
+++ Meta-L.cpp
@@ -1,77 +1,24 @@
-#include <stdio.h>
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-// An unsigned char can store 1 Bytes (8bits) of data (0-255)
-typedef unsigned char BYTE;
-
-const char *cPath = "C:\\Users\\loxa winbox\\Desktop\\Meta-L\\HelloWorldTest.exe";	
-const char *cPathW = "C:\\Users\\loxa winbox\\Desktop\\Meta-L\\HelloWorldTest2.exe";
-
-BYTE *bFileBuffer;			
-FILE *fFile = NULL;	
-FILE *fFileW = NULL;	
-
-long fileSize;
-
-//**********************************************************
-
-
-BYTE* _remNOP_()
-{
-     //NULLs most NOPs (0x90->0x0)
-     long lFileBufferSize = fileSize;
-     for(int i=0;i<=lFileBufferSize+1;i++)
-     {
-        if((bFileBuffer[i]==0x90) && (bFileBuffer[i+1]==0x90))
-            {bFileBuffer[i]=0x0; bFileBuffer[i+1]=0x0;}
-     }
-}
-
-long _fFileSize(FILE *file)
-{     
-	long lCurPos, lEndPos;
-	lCurPos = ftell(file);
-	fseek(file, 0, 2);
-	lEndPos = ftell(file);
-	fseek(file, lCurPos, 0);
-	return lEndPos;
-}
-
-void _fOpenRead()
-{
-    if ((fFile = fopen(cPath, "rb+")) == NULL){;} else {_NOACTION_();}
-}
-
-void _fOpenWrite()
-{
-    if ((fFileW = fopen(cPathW, "wb+")) == NULL){;} else {_NOACTION_();}
-}
-
-void _fWriteFile()
-{
-    _fOpenWrite();
-    fwrite(bFileBuffer, 1, fileSize, fFileW);     
-}
-
-
-void _fPrint(BYTE *buffer, int count )
-{
-        for (int i = 0; i < count; i++)     
-             cout << buffer[i];
-}
+#include "Functions.h"
+#include "Parser.h"
 
 int main()
 {
-	_fOpenRead();
-
-	fileSize = _fFileSize(fFile);
+    //Objects
+    Functions Functions;
+    Parser Parser;
+    
+    
+    //Temporary
+    //cPath = ".\\HelloWorldTest.exe";
+    //cPathW = ".\\HelloWorldTest2.exe";
+        
+    
+   	Functions._fOpenRead();
+	fileSize = Functions._fFileSize(fFile);
 	bFileBuffer = new BYTE[fileSize];
 	fread(bFileBuffer, fileSize, 1, fFile);
-	_remNOP_();
-	_fWriteFile();
+	Functions._remNOP_();
+	Functions._fWriteFile();
 	//_fPrint(bFileBuffer,500);
 
 	cin.get();
@@ -80,5 +27,7 @@ int main()
     fclose(fFile);   
     fclose(fFileW); 
     
+    
 	system("pause");
+
 }
